2473d5711e991546b7de03f622750830604539f3
SergeyZaprudnov <szaprudnov@hotmail.com>
2023-09-09 17:57:47 +0300

    settings and descriptions of models have been added

--- skymarket/users/models.py
+++ skymarket/users/models.py
@@ -1,16 +1,55 @@
 from django.contrib.auth.models import AbstractBaseUser, AbstractUser
 from django.db import models
-from users.managers import UserManager
+from skymarket.users.managers import UserManager
 from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.translation import gettext_lazy as _
 
 
-class UserRoles:
-    # TODO закончите enum-класс для пользователя
-    pass
+class UserRoles(models.TextChoices):
+    USER = 'user', 'USER'  # Пользователь
+    ADMIN = 'admin', 'Administrator'  # Администратор
 
 
 class User(AbstractBaseUser):
-    # TODO переопределение пользователя.
-    # TODO подробности также можно поискать в рекоммендациях к проекту
-    pass
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone', 'role']
+
+    objects = UserManager()
+
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    last_name = models.CharField(max_length=100, verbose_name='Фамилия')
+    phone = PhoneNumberField(unique=True, verbose_name='Номер телефона')
+    email = models.EmailField(unique=True, verbose_name='Электронная почта')
+    role = models.CharField(max_length=15, choices=UserRoles.choices, default=UserRoles.USER,
+                            verbose_name='Роль пользователя')
+    image = models.ImageField(upload_to='avatars/', blank=True, null=True, verbose_name='Аватарка')
+    is_active = models.BooleanField(default=True, verbose_name='Действующий')
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    @property
+    def is_admin(self):
+        return self.role == UserRoles.ADMIN
+
+    @property
+    def is_user(self):
+        return self.role == UserRoles.USER
+
+    @property
+    def is_superuser(self):
+        return self.is_admin
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
