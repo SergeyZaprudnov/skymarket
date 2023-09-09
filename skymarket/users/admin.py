24a2cd250daf6f46bb14856fe3c69c24d0033c03
SergeyZaprudnov <szaprudnov@hotmail.com>
2023-09-09 18:59:37 +0300

    admin panel description

--- skymarket/users/admin.py
+++ skymarket/users/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group as BaseGroup
 
-from users.models import User
+from .models import User
+
+
 # TODO Aдмика для пользователя - как реализовать ее можно подсмотреть в документаци django
 # TODO Обычно её всегда оформляют, но в текущей задачи делать её не обязательно
+admin.site.register(User)
