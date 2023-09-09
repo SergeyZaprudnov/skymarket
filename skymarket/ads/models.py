4c5f4ba69596d162a38553bad8a4aac01b521e69
SergeyZaprudnov <szaprudnov@hotmail.com>
2023-09-10 00:13:33 +0300

    added description of models

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -1,21 +1,18 @@
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 NULLABLE = {'blank': True, 'null': True}
 
 
 class Ad(models.Model):
-    title = models.CharField(max_length=250, verbose_name='Название')
-    price = models.PositiveIntegerField(verbose_name='Цена')
+    title = models.CharField(max_length=250, verbose_name='Название', default='')
+    price = models.PositiveIntegerField(verbose_name='Цена', **NULLABLE)
     description = models.TextField(verbose_name='Описание', blank=True)
     image = models.ImageField(verbose_name='Изображение', upload_to='ads', **NULLABLE)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Автор',
-                               related_name='abs')
-    created_at = models.DateTimeField(verbose_name='Дата и время создания', auto_now_add=True)
-
-    class Meta:
-        verbose_name = 'Объявление'
-        verbose_name_plural = 'Объявления'
+                               related_name='abs', default=1)
+    created_at = models.DateTimeField(verbose_name='Дата и время создания', default=timezone.now)
 
     def __str__(self):
         result = self.title[:50]
@@ -23,20 +20,23 @@ class Ad(models.Model):
             result += '...'
         return result
 
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+
 
 class Comment(models.Model):
-    text = models.CharField(max_length=1500, verbose_name='Описание')
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Автор',
-                               related_name='comments')
-    ad = models.ForeignKey(Ad, on_delete=models.CASCADE, verbose_name='Объявление', related_name='comments')
-    create_at = models.DateTimeField(verbose_name='Дата и время создания', auto_now_add=True)
-
-    class Meta:
-        verbose_name = 'Комментарий'
-        verbose_name_plural = 'Коментарии'
+    text = models.CharField(max_length=1500, verbose_name='Название', default='')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, null=True,  on_delete=models.CASCADE, verbose_name='Автор')
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE, null=True,  verbose_name='Объявление')
+    create_at = models.DateTimeField(verbose_name='Дата и время создания', default=timezone.now)
 
     def __str__(self):
         result = self.text[:50]
         if len(self.text) > 50:
             result += '...'
         return result
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
