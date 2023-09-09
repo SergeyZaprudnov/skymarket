cf157aaba363113b3219aa3e47e0900e2aeb0b03
SergeyZaprudnov <szaprudnov@hotmail.com>
2023-09-09 19:00:06 +0300

    adding URLs

--- skymarket/skymarket/urls.py
+++ skymarket/skymarket/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 # TODO здесь необходимо подклюючит нужные нам urls к проекту
 
 urlpatterns = [
-    path("api/admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
     path("api/redoc-tasks/", include("redoc.urls")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
