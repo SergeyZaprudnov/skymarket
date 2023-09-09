69a4665f033bab2c1b676b8c699756760c995e4f
SergeyZaprudnov <szaprudnov@hotmail.com>
2023-09-09 17:31:16 +0300

    database settings added

--- skymarket/skymarket/settings.py
+++ skymarket/skymarket/settings.py
@@ -92,6 +92,14 @@ DJOSER = {
 
 # TODO здесь необходимо настроить подключение к БД
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
 }
 
 
