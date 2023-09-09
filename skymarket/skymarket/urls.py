a03d538908be58486dd1e89ccfcf3c77686f0af1
SergeyZaprudnov <szaprudnov@hotmail.com>
2023-09-09 19:23:23 +0300

    description of URLs

--- skymarket/skymarket/urls.py
+++ skymarket/skymarket/urls.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 # TODO здесь необходимо подклюючит нужные нам urls к проекту
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/admin/", admin.site.urls),
     path("api/redoc-tasks/", include("redoc.urls")),
+    path("api/", include("users.urls")),
+    path("api/", include("ads.urls")),
+    path("api/token/", TokenObtainPairView.as_view()),
+    path("api/refresh/", TokenRefreshView.as_view()),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
