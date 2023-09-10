eb2590ad5d915cedd8794c6d816dfc1b7b6174f6
SergeyZaprudnov <szaprudnov@hotmail.com>
2023-09-10 23:48:50 +0300

    editing

--- skymarket/skymarket/settings.py
+++ skymarket/skymarket/settings.py
@@ -108,11 +108,11 @@ DJOSER = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'skymarket',
-        'USER': 'postgres',
-        'PASSWORD': 'sixteen',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
