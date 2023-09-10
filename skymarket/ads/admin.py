b7edefe5d70a5b87dce6a621129157c79403875e
SergeyZaprudnov <szaprudnov@hotmail.com>
2023-09-10 22:58:24 +0300

    editing

--- skymarket/ads/admin.py
+++ skymarket/ads/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from .models import Ad, Comment
 
-admin.site.register(Ad)
-admin.site.register(Comment)
+
+# TODO здесь можно подкючить ваши модели к стандартной джанго-админке
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'price', 'author')
+    list_filter = ('author',)
+    search_fields = ('title', 'description')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'ad', 'author')
+    list_filter = ('author',)
